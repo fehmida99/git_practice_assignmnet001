6e5cf3010314536c995e8e5b0a888a2ad1675232
Neha Jaiswal <nehaas1900@gmail.com>
2022-09-20 21:51:21 +0530

    update_prime_code

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let num = 13
 let count=0
 for(let i=1; i<=num; i++){
-	if(num%i==1){
+	if(num%i==0){
 		count++
 	}
 	if(count==2){
 		console.log("Yes"){
 		}else{
-		console.log("No)}
+		console.log("No")}
 				
